9f10e6268d5922b58828524418d1af5eca390059
k-gintaras <emnomage@yahoo.com>
2020-11-21 19:12:01 +0000

    added css

--- musicorator/src/app/anti-spam-click.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AntiSpamClickDirective } from './anti-spam-click.directive';
-
-describe('AntiSpamClickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AntiSpamClickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
